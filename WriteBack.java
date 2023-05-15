0125ab9c2384c300390cfdbe59c46f73b751694d
Omar Ahmed <67840386+omarafekry@users.noreply.github.com>
2023-05-15 22:56:23 +0200

    fixed writeback bug

--- WriteBack.java
+++ WriteBack.java
@@ -1,6 +1,6 @@
 public class WriteBack extends PPPart{
     Instruction currInstruction = null;
-    int toPrint = 0, counter = 0;
+    int toPrint = 0, counter = 0, printRegister, printValue;
     boolean hasOutput = false, outputNextCycle = false;
     WriteBack(){
         neededCycles = 1;
@@ -12,10 +12,8 @@ public class WriteBack extends PPPart{
             hasOutput = true;
             outputNextCycle = false;
             instructionID = -1;
-        }
-        if (toPrint > 0){
-            //System.out.println("WRITING BACK INSTRUCTION " + counter);
-            toPrint--;
+            if (printRegister != -1 )System.out.println("Set value of R" + printRegister + " to " + printValue);
+            printRegister = -1;
         }
     }
     public void writeback(Register[] Registers, Instruction inst) {
@@ -23,9 +21,10 @@ public class WriteBack extends PPPart{
     	int operation = Integer.parseInt(currInstruction.OpCode,2);
     	
     	
-    	if( operation!= 4 || operation != 7 || operation!= 11) {
-    		
+    	if( operation!= 4 && operation != 7 && operation!= 11) {
     		Registers[currInstruction.AddrR1].value = currInstruction.ALUoutput;
+            printRegister = currInstruction.AddrR1;
+            printValue = currInstruction.ALUoutput;
     	}
         toPrint++;
         counter++;
